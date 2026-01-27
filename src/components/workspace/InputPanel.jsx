54188afd3d92b456240b21bde46a4c9b185fb33d
meepmeep <elijahsabay@gmail.com>
2026-01-28 02:14:38 +0800

    added websearcher

--- src/components/workspace/InputPanel.jsx
+++ src/components/workspace/InputPanel.jsx
@@ -2,12 +2,14 @@ import ExtractorInput from "./input/ExtractorInput.jsx";
 import SummarizerInput from "./input/SummarizerInput.jsx";
 import GapInput from "./input/GapInput.jsx";
 import TopicSuggesterInput from "./input/TopicSuggesterInput.jsx";
+import SearcherInput from "./input/SearcherInput.jsx";
 
 const STEP_INPUT_COMPONENTS = {
   extractor: ExtractorInput,
   summarizer: SummarizerInput,
   gap: GapInput,
   topic: TopicSuggesterInput,
+  search : SearcherInput
 };
 
 export default function InputPanel({ step }) {
--- src/components/workspace/ResultPanel.jsx
+++ src/components/workspace/ResultPanel.jsx
@@ -35,11 +35,13 @@ import SummarizerResult from "./output/SummarizerResult";
 import ExtractorOutput from "./output/ExtractorOutput";
 import GapExtractorOutput from "./output/GapOutput";
 import TopicSuggesterOutput from "../workspace/output/TopicSuggesterOutput.jsx";
+import SearcherOutput from "../workspace/output/SearcherOutput.jsx";
 const STEP_INPUT_COMPONENTS = {
   extractor: ExtractorOutput,
   summarizer: SummarizerResult,
   gap: GapExtractorOutput,
-  topic: TopicSuggesterOutput
+  topic: TopicSuggesterOutput,
+  search: SearcherOutput
 };
 
 export default function InputPanel({ step }) {
